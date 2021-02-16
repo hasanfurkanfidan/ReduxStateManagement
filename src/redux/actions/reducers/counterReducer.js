90c966e4fd7c0de0f3e0ab3f01bd6bc821a9f1a5
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-02-16 19:20:24 +0300

    ilerleme sağlandı

---
+++ src/redux/actions/reducers/counterReducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from "../actionTypes"
+
+const counterReducer=(state=0,action)=>{
+    let newState;
+    switch (action.type) {
+        case actionTypes.INCREASE_COUNTER
+            
+            break;
+    
+        default:
+            break;
+    }
+}
